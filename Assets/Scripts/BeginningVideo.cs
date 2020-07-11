703a93eacd7c043542bd3c2751f000057cf5d221
Xela20S <alex.abe.short@gmail.com>
2020-07-11 00:15:47 -0700

    Many Updates
    -Level1
    -IntroScene
    -Art
    -BasicGround TilePalette
    -Level Select

---
+++ Assets/Scripts/BeginningVideo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class BeginningVideo : MonoBehaviour
+{
+    VideoPlayer vd;
+    public string sceneToLoad;
+    private bool canFinish;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        vd = this.gameObject.GetComponent<VideoPlayer>();
+        vd.Play();
+        canFinish = false;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(!vd.isPlaying && canFinish)
+        {
+            SceneManager.LoadSceneAsync(sceneToLoad);
+        }
+
+        if(vd.isPlaying)
+        {
+            canFinish = true;
+        }
+    }
+    
+}
---
+++ Assets/Scripts/LevelButtonScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelButtonScript : MonoBehaviour
+{
+    private GameObject Manager;
+    private GlobalStorage storage;
+    private SpriteRenderer myTexture;
+    public int Level;
+    public string Load;
+    public Sprite lockedTexture;
+    public Sprite unlocked_Texture;
+    void Start()
+    {
+        Manager = GameObject.Find("GameManager");
+        storage = Manager.gameObject.GetComponent<GlobalStorage>();
+        myTexture = this.gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(storage.unlocked >= Level)
+        {
+            myTexture.sprite = unlocked_Texture;
+        }
+        else
+        {
+            myTexture.sprite = lockedTexture;
+        }
+    }
+
+    public void OnClick()
+    {
+        if(storage.unlocked >= Level)
+        {
+            SceneManager.LoadSceneAsync(Load);
+        }
+    }
+}
